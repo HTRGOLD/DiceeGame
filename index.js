890a258c147d309b7c2f845ee3a4cbca50ab310b
HTRGOLD <54968893+HTRGOLD@users.noreply.github.com>
2021-12-28 21:30:36 -0800

    Added files of my project to Github

---
+++ index.js
@@ -0,0 +1,15 @@
+let diceImages = ["images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png"];
+
+var randomNumber1 = diceImages[Math.floor((Math.random() * 6))];
+var randomNumber2 = diceImages[Math.floor((Math.random() * 6))];
+
+document.querySelector(".img1").setAttribute("src", randomNumber1);
+document.querySelector(".img2").setAttribute("src", randomNumber2);
+
+if (randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 Wins!"
+} else if (randomNumber2>randomNumber1){
+    document.querySelector("h1").innerHTML = "Player 2 Wins!"
+} else if (randomNumber2 === randomNumber1) {
+    document.querySelector("h1").innerHTML = "It's a draw!"
+}
